11e87fdce4c628721a51a02b131d3cb979f23c61
xaedes <xaedes@googlemail.com>
2015-04-19 22:24:50 +0200

    move left and right

---
+++ Assets/Scripts/ControlMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlMovement : MonoBehaviour {
+	Movement movement;
+
+	void Awake () {
+		movement = GetComponent <Movement>();
+	}
+	
+	
+	void FixedUpdate()
+	{
+		// Store the input axes.
+		float h = Input.GetAxisRaw("Horizontal");
+		
+		// Move the player around the scene.
+		movement.Move(h, 0);
+		
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+	Vector3 movement;
+	public float speed = 1f; 
+	const float movingThreshold = 1e-2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate()
+	{
+		// Move the entity around the scene according to this.movement
+		Move();
+		
+		// Movement for this circle done.
+		movement.Set(0, 0, 0);
+		
+	}
+
+	public void Move(float h, float v) {
+		Move(new Vector3(h,v,0f));
+	}
+	public void Move(Vector3 v) {
+		movement += v;
+	}
+
+	void Move()
+	{
+		// Move the player to it's current position plus the movement.
+		
+		if (movement.magnitude > movingThreshold) {
+			float length = Time.deltaTime * speed;
+			
+			if( movement.magnitude < length ) {
+				length = movement.magnitude;
+			}
+			movement = movement.normalized * length;
+		} else {
+			movement = Vector3.zero;
+		}
+		
+		Vector3 update = transform.position + movement;
+		transform.position = update;
+	}
+
+}
